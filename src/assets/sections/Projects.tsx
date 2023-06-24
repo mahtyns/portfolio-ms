217ad3d88bb6960b5b23c2a57f8b96be1ff5a5f1
Martyna <m.smolarek19@gmail.com>
2023-06-24 13:48:12 +0200

    project settings

--- src/assets/sections/Projects.tsx
+++ src/assets/sections/Projects.tsx
@@ -1,24 +1,27 @@
+import { useState } from "react"
 import ActiveCircle from "../components/ActiveCircle"
 import ProjectCardGrid from "../components/ProjectCardGrid"
 import ProjectInfo from "../components/ProjectInfo"
 import {SectionTitle} from "../components/SectionTitle"
 import useMediaQueries from "../hooks/useMediaQueries"
+import { projects } from "../datafiles/data"
 
 const Projects = () => {
 
     const isAboveMediumScreen = useMediaQueries('(min-width: 1240px)')
+    const [projectIndShown, setProjectIndShown] = useState(0)
 
     return (
         <section id="projects" className={`bg-gray-100 xl:h-module-big`}>
             <div className={'flex xl:flex-row flex-col h-full w-full'}>
                 {isAboveMediumScreen
                     ? <div className="w-full py-12 px-12 xl:w-1/2 xl:py-36 xl:px-36 ">
-                            <ProjectCardGrid/>
-                        </div>
+                          <ProjectCardGrid projects={projects} projectIndShown={projectIndShown} />
+                      </div>
                     : null}
                 <div className="w-full py-16 px-16 xl:w-1/2 bg-gray-50 h-full xl:py-36 xl:px-36">
                     <SectionTitle sectionTitle="My work." sectionVariant={true}/>
-                    <ProjectInfo/>
+                    <ProjectInfo projects={projects} projectIndShown={projectIndShown} />
                     {isAboveMediumScreen ? null : <div className="flex w-full justify-center gap-4 pt-4">
                         <ActiveCircle />
                         <ActiveCircle />
