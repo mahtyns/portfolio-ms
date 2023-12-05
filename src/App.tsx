ccf5e9e2a3ba95bbc9fe8ce6addd56c2d0968a65
Martyna <m.smolarek19@gmail.com>
2023-12-05 17:43:57 +0100

    dark mode state

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,22 @@ import Home from "./assets/sections/Home"
 import Projects from "./assets/sections/Projects"
 import Technology from "./assets/sections/Technology"
 import Footer from "./assets/sections/Footer"
+import { useEffect, useState } from "react"
+
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect( 
+    () => {
+    document.documentElement.classList.toggle('dark', darkMode)
+    }, [darkMode]
+  )
+
+  const handleDarkMode = () => {
+    setDarkMode(!darkMode)
+  }
   
   return (
     <>
