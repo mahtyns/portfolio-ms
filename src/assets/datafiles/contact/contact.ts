7c584afad9c99415fa6696f9a51ee2152052a3d0
Martyna <m.smolarek19@gmail.com>
2025-07-20 15:52:06 +0200

    update(20-07): update contact component

---
+++ src/assets/datafiles/contact/contact.ts
@@ -0,0 +1,1 @@
+export const contactMe = "I'm always looking for a new challenge and possibilities of learning and growing. Want me in your team? Feel free to contact me and ask me anything you need to know."
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,21 +1,18 @@
 import { SectionTitle } from "../assets/components/basics/SectionTitle"
-import { contactMe } from "../assets/datafiles/data"
+import { contactMe } from "../assets/datafiles/contact/contact"
 
 const Contact = () => {
 
   return (
-    <section  id="contact" className={'h-module-medium p-16 xl:py-36 xl:px-36 dark:bg-main-dark'}>
-      <div className="xl:w-1/2">
+    <section  id="contact" className={'contact dark:bg-main-dark'}>
+      <div className="contact__wrapper">
         <SectionTitle sectionTitle="Let's talk." sectionVariant={true}/>
-        <p className="xl:w-5/6 py-8 font-lato  tracking-wide font-light text-lg">
+        <p className="contact__description">
           {contactMe}
         </p>
-        <p className="xl:w-5/6 font-lato tracking-wide font-light text-lg">
-          Email: <span className="font-medium"><a href="mailto:m.smolarek19@gmail.com">m.smolarek19@gmail.com</a></span>
+        <p className="contact__link">
+          Email: <span className="contact__link--bold"><a href="mailto:m.smolarek19@gmail.com">m.smolarek19@gmail.com</a></span>
         </p>
-      </div>
-      <div className="xl:w-1/2">
-           
       </div>
     </section>
   )
