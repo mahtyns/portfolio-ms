ba94898723a862410a3a4bb699fbdb56ff35be3d
Martyna <m.smolarek19@gmail.com>
2026-05-10 16:53:56 +0200

    feat: update navbar

--- src/assets/components/basics/Navlink.tsx
+++ src/assets/components/basics/Navlink.tsx
@@ -3,19 +3,24 @@ import AnchorLink from 'react-anchor-link-smooth-scroll'
 type Props = {
     pageName: string
     clickedPage: string
-    setMenuToggled: (value: boolean)=>void
+    currentPage?: string
+    setMenuToggled: (value: boolean) => void
 }
 
 export const Navlink = (props: Props) => {
-  
+
+    const isActive = props.currentPage === props.clickedPage;
+
     return (
-    <AnchorLink 
-    href={`#${props.clickedPage}`} 
-    className={`navlink dark:text-gray-200`}
-    onClick={()=>props.setMenuToggled(false)}>
-        {props.pageName}
-    </AnchorLink>
-  )
+        <AnchorLink
+            href={`#${props.clickedPage}`}
+            className={`navlink`}
+            aria-label={`Go to ${props.pageName} section`}
+            aria-current={isActive ? 'page' : undefined}
+            onClick={() => props.setMenuToggled(false)}>
+            {props.pageName}
+        </AnchorLink>
+    )
 }
 
 export default Navlink
